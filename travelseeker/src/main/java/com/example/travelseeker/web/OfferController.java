bc28fdd74571ae4769efef429021a99c7dc425bc
LazarDukov <lazarrumenowdukow@gmail.com>
2023-10-04 19:15:58 +0300

    added some html files

--- travelseeker/src/main/java/com/example/travelseeker/web/OfferController.java
+++ travelseeker/src/main/java/com/example/travelseeker/web/OfferController.java
@@ -2,20 +2,27 @@ package com.example.travelseeker.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/offers")
 public class OfferController {
-    @GetMapping("/offers/airplane-tickets")
+    @GetMapping("/add-offer")
+    public String addOffer() {
+        return "add-offer";
+    }
+
+    @GetMapping("/airplane-tickets")
     public String getAirplaneTicket() {
         return "airplane-tickets";
     }
 
-    @GetMapping("/offers/cars")
+    @GetMapping("/cars")
     public String getCars() {
         return "cars";
     }
 
-    @GetMapping("/offers/hotels")
+    @GetMapping("/hotels")
     public String getHotels() {
         return "hotels";
     }
