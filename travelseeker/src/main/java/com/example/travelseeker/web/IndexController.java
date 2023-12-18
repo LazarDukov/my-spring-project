ff020d9d7146b3c845a7f57d1260d10c4f00e6ec
LazarDukov <lazarrumenowdukow@gmail.com>
2023-12-18 09:02:00 +0200

    index page view almost done

--- travelseeker/src/main/java/com/example/travelseeker/web/IndexController.java
+++ travelseeker/src/main/java/com/example/travelseeker/web/IndexController.java
@@ -1,12 +1,29 @@
 package com.example.travelseeker.web;
 
+import com.example.travelseeker.service.AirplaneTicketsService;
+import com.example.travelseeker.service.CarRentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    private final AirplaneTicketsService airplaneTicketsService;
+    private final CarRentService carRentService;
+
+    public IndexController(AirplaneTicketsService airplaneTicketsService, CarRentService carRentService) {
+        this.airplaneTicketsService = airplaneTicketsService;
+
+        this.carRentService = carRentService;
+    }
+
     @GetMapping("/")
-    public String getIndex() {
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("indexAirplaneTickets", this.airplaneTicketsService.getAllAirplaneTickets());
+        model.addAttribute("indexCars", this.carRentService.getAllCars());
+        model.addAttribute("indexHotels", this.carRentService.getAllCars());
+
         return "index";
     }
 
